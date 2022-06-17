21fd7a996fa867a30a0b0bc4f1d6189126db6e0a
kalenshi <kalenshi@gmail.com>
2022-06-17 07:56:03 -0300

    Updated database location to amazon aws/created a config dict

--- api/products/list_view.py
+++ api/products/list_view.py
@@ -1,7 +1,6 @@
-from rest_framework import status
-from rest_framework.response import Response
+from drf_yasg import openapi
 from rest_framework.views import APIView
-
+from drf_yasg.utils import swagger_auto_schema
 from api.models import Products
 from api.products.pagination import ProductPagination
 from api.products.serializers import ProductsSerializer
@@ -10,10 +9,29 @@ from api.products.serializers import ProductsSerializer
 class ProductsListView(APIView):
     """
     View for interacting with the product model without id
+    based on the provided filters
     """
     serializer_class = ProductsSerializer
     pagination_class = ProductPagination
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name="productcode",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Product code",
+                require=False
+            ),
+            openapi.Parameter(
+                name="productname",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Product code",
+                require=False
+            ),
+        ]
+    )
     def get(self, request, format=None):
         """
         Retrieve a list of products from the database
